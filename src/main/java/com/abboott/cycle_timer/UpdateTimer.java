42c8ecaaa6f04b388253185c0b285e0d6ac3ba5d
Viktor Lukashev <viktor.lukashev@veropharm.ru>
2023-07-23 18:28:34 +0300

    класс для переодического обновления задачи

---
+++ src/main/java/com/abboott/cycle_timer/UpdateTimer.java
@@ -0,0 +1,32 @@
+package com.abboott.cycle_timer;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class UpdateTimer {
+    private Timer timer = new Timer();
+
+    public void startFetchingData(String t) {
+        // Создаем таймер и задачу для выполнения кода каждую минуту
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                if (t != null) {
+                    System.out.println(t);
+                }
+            }
+        };
+
+        // Запускаем таймер
+        timer.schedule(task, 0, 6000);
+    }
+
+    // Для остановки таймера
+    public void stopFetchingData() {
+        // Останавливаем таймер
+        timer.cancel();
+        timer.purge();
+        System.out.println("Таймер остановлен.");
+    }
+}
