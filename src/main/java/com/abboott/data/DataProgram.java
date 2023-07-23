348a06c4294182bc1423d56111c9d589292e52a4
Viktor Lukashev <viktor.lukashev@veropharm.ru>
2023-07-23 17:27:58 +0300

    создал общий класс для переременных

---
+++ src/main/java/com/abboott/data/DataProgram.java
@@ -0,0 +1,10 @@
+package com.abboott.data;
+
+public class DataProgram {
+    public static final String URL_WEB_SITE = "https://devmark.ru/";
+    public static final String CSS_SELECTOR = "div";
+    public static final int[] SIZE_WINDOW = {650, 400};
+    public static final String[] COLUMN_NAMES_WINDOWS = {"First Name", "Last Name", "Age"};
+    public static final String BUTTON_EXIT = "EXIT";
+    public static final String FRAME_TABLE = "Visual Task";
+}
