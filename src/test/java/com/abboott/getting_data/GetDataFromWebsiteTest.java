61d2f0ecc9bf3afc63836eacb86ecfeaa246f34e
Viktor Lukashev <viktor.lukashev@veropharm.ru>
2023-07-23 19:01:00 +0300

    Создал тестовый класс для GetDataFromWebsite.java

---
+++ src/test/java/com/abboott/getting_data/GetDataFromWebsiteTest.java
@@ -0,0 +1,30 @@
+package com.abboott.getting_data;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GetDataFromWebsiteTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void parseDataFromSite() {
+        GetDataFromWebsite getDataFromWebsite = new GetDataFromWebsite();
+        System.out.println(getDataFromWebsite.parseDataFromSite());
+    }
+
+    @Test
+    void searchElement() {
+        GetDataFromWebsite getDataFromWebsite = new GetDataFromWebsite();
+        System.out.println(getDataFromWebsite.searchElement());
+    }
+}
